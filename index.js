8e5f126dee59292db371c62e382c1b21efa9ea1f
Kir-aaa <kirak4063@gmail.com>
2026-02-10 09:04:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ function changeCakeImage()
 function  changeGiftImage()
 { 
     giftMessage = document.getElementById('giftMessage');
-    giftMessage.innerText = "Oh I forgot what these are called, but because you seem to like them very much, I made you into them"
+    giftMessage.innerText = "Oh I forgot what these are called, but because you seem to like them very much, I made you into them";
     let giftImg = document.getElementById("myGift");
     giftImg.src = "images/fuggley.png";
     return false;
 
   
 }
+
 
